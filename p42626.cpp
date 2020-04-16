0ab0bba14f16fdf6732c994cd931fbf3f41fa12b
Jung Sim <tlawjd99gma@gmail.com>
2020-04-16 22:50:03 +0900

    p42748 solved

---
+++ p42626.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+	int answer = 0;
+	return answer;
+}
+
+int main() {
+	vector<int> v1 = { 1, 2, 3, 9, 10, 12 };
+	vector<int> v2 = { 3, 9, 9, 3, 5, 7, 2 };
+	int i1 = 7;
+	int i2 = 0;
+	cout << solution(v1, i1) << '\n';
+}
---
+++ p42748.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+	vector<int> answer;
+	vector<int> temp;
+
+	for (int i = 0; i < commands.size(); i++) {
+		temp.clear();
+		temp = array;
+		sort(temp.begin() + commands[i][0] - 1, temp.begin() + commands[i][1]);
+		answer.push_back(temp[commands[i][0] + commands[i][2] - 2]);
+
+		for (int j = 0; j < array.size(); j++) {
+			cout << temp[j] << ' ';
+
+		}
+		cout << '\n';
+	}
+	
+	return answer;
+}
+
+int main() {
+	vector<int> v1 = { 1, 5, 2, 6, 3, 7, 4 };
+	vector<vector<int>> v2 = { {2, 5, 3}, {4, 4, 1}, {1, 7, 3} };
+	int i1 = 0;
+	int i2 = 0;
+	vector<int> ans = solution(v1, v2);
+
+	for (int i = 0; i < ans.size(); i++)
+		cout << ans[i] << ' ';
+	cout << '\n';
+}
