f29d736cf035fefa2e44f550e679059fe9fe78b2
Jung Sim <tlawjd99gma@gmail.com>
2020-04-20 22:00:41 +0900

    p42840 solved

--- p42840.cpp
+++ p42840.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -8,8 +9,10 @@ using namespace std;
 
 vector<int> solution(vector<int> heights) {
 	vector<int> answer;
+	vector<pair<int, int>> temp;
 
-	int n1 = 0, n2 = 0, n3 = 0, a1 = 1, a2 = 1, a3 = 3;
+	bool two = true;
+	int n1 = 0, n2 = 0, n3 = 0, a1 = 1, a2 = 5, a3 = 3;
 	for (int i = 0; i < heights.size(); i++) {
 		if (heights[i] == a1) {
 			n1++;
@@ -19,11 +22,66 @@ vector<int> solution(vector<int> heights) {
 			a1 = 1;
 	}
 
+	for (int i = 0; i < heights.size(); i++) {
+		if (!two) {
+			if (heights[i] == a2) {
+				n2++;
+			}
+		}
+		else {
+			if (heights[i] == 2) {
+				n2++;
+			}
+		}
+		
+		if (two) {
+			a2++;
+			if (a2 == 2)
+				a2++;
+			else if (a2 == 6) {
+				a2 = 1;
+			}
+		}
+		two = !two;
+	}
+
+	two = false;
+	for (int i = 0; i < heights.size(); i++) {
+		if (heights[i] == a3) {
+			n3++;
+		}
+
+		if (two) {
+			a3++;
+			if (a3 == 4) {
+				a3 = 1;
+			}
+			else if (a3 == 3) {
+				a3 = 4;
+			}
+			else if (a3 == 6)
+				a3 = 3;
+		}
+		two = !two;
+	}
+
+	temp.push_back(make_pair(n1, 1));
+	temp.push_back(make_pair(n2, 2));
+	temp.push_back(make_pair(n3, 3));
+	sort(temp.begin(), temp.end());
+
+	if (temp[0].first == temp[2].first)
+		answer.push_back(temp[0].second);
+	if (temp[1].first == temp[2].first)
+		answer.push_back(temp[1].second);
+	answer.push_back(temp[2].second);
+
+
 	return answer;
 }
 
 int main() {
-	vector<int> v1 = { 1, 2, 3, 4, 5 };
+	vector<int> v1 = { 1, 3, 2, 4, 2 };
 	vector<int> v2 = { 3, 9, 9, 3, 5, 7, 2 };
 	int i1 = 0;
 	int i2 = 0;
