25fe1e03685a5767e4255a53a3ce062ad8734c0d
Jung Sim <tlawjd99gma@gmail.com>
2020-04-25 23:40:50 +0900

    p42626 solved

--- p42626.cpp
+++ p42626.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 int solution(vector<int> scoville, int K) {
 	int answer = 0;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < scoville.size(); i++) {
+		pq.push(scoville[i]);
+	}
+
+	while (pq.top() < K) {
+		if (pq.size() < 2) {
+			return -1;
+		}
+		int newone = pq.top();
+		pq.pop();
+
+		newone += pq.top() * 2;
+		pq.pop();
+
+		pq.push(newone);
+		answer++;
+	}
 	return answer;
 }
 
