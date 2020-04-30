857872f66d502bebe3074b76f625d82704c68bb3
Jung Sim <tlawjd99gma@gmail.com>
2020-04-30 11:36:42 +0900

    p43237 solved

--- p43237.cpp
+++ p43237.cpp
@@ -6,26 +6,62 @@
 using namespace std;
 
 int solution(vector<int> budgets, int M) {
-	int answer = 0;
+	long long answer = 0, right, left, mid, sum;
 
 	sort(budgets.begin(), budgets.end());
-	int everage = M / 4, sum=0, idx = 0;
-	auto j = upper_bound(budgets.begin(), budgets.end(), everage);
-	for (auto i = budgets.begin(); i != j; i++) {
-		sum += everage - *i;
-		idx++;
+	left = 0;
+	right = M;
+	sum = 0;
+	for (int i = 0; i < budgets.size(); i++) {
+		sum += budgets[i];
 	}
 
-	if (budgets.size() - idx)
-		answer = everage + sum / (budgets.size() - idx);
-	else
-		answer = everage;
+	if (sum < M) {
+		return budgets[budgets.size() - 1];
+	}
+	else {
+		while (left <= right) {
+			mid = (left + right) / 2;
+			answer = mid;
+			sum = 0;
+
+			for (int i = 0; i < budgets.size(); i++) {
+				if (budgets[i] > mid) {
+					sum += mid;
+				}
+				else {
+					sum += budgets[i];
+				}
+			}
+
+			if (sum < M) {
+				left = mid + 1;
+			}
+			else if (sum > M) {
+				right = mid - 1;
+				answer = right;
+			}
+			else {
+				answer = mid;
+				break;
+			}
+		}
+	}
+
 	return answer;
 }
 
 int main() {
-	vector<int> v1 = { 121, 110, 140, 150 };
-	int i1 = 485;
+	vector<int> v1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int i1 = 56;
+
+	vector<int> v2 = { 110, 120, 100, 100};
+	int i2 = 450;
+
+	vector<int> v3 = { 120, 110, 140, 150 };
+	int i3 = 485;
 
 	cout << solution(v1, i1) << '\n';
+	cout << solution(v2, i2) << '\n';
+	cout << solution(v3, i3) << '\n';
 }
